dd644d6f2c2ccce996c1c55c4b00f6793381eb6c
André Silveira <andre.silveira1silva@gmail.com>
2025-01-07 22:30:46 -0300

    feat: add create account controller

---
+++ src/http/controllers/create-account.ts
@@ -0,0 +1,30 @@
+import { CreateAccountUseCase } from '@/use-cases/create-account'
+import type { FastifyReply, FastifyRequest } from 'fastify'
+import * as zod from 'zod'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
+
+const registerBodySchema = zod.object({
+  name: zod.string(),
+  email: zod.string().email(),
+  password: zod.string().min(6),
+})
+
+export async function createAccount(req: FastifyRequest, reply: FastifyReply) {
+  const { name, email, password } = registerBodySchema.parse(req.body)
+
+  try {
+    const prismaUsersRepository = new PrismaUsersRepository()
+    const createAccountUseCase = new CreateAccountUseCase(prismaUsersRepository)
+
+    await createAccountUseCase.execute({ name, email, password })
+  } catch (err) {
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: err.message })
+    }
+
+    return reply.status(500).send()
+  }
+
+  return reply.status(201).send()
+}
--- src/http/server.ts
+++
@@ -1,6 +0,0 @@
-import { env } from '@/env'
-import { app } from './app'
-
-app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
-  console.log('🚀 HTTP Server Running!')
-})
