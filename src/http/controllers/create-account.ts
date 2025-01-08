bdf29f932ce9eeaaed5f55a8377ab7fd8f776bdd
André Silveira <andre.silveira1silva@gmail.com>
2025-01-08 20:26:56 -0300

    feat: handle with global errors

--- src/http/controllers/create-account.ts
+++ src/http/controllers/create-account.ts
@@ -23,7 +23,7 @@ export async function createAccount(req: FastifyRequest, reply: FastifyReply) {
       return reply.status(409).send({ message: err.message })
     }
 
-    return reply.status(500).send()
+    throw err
   }
 
   return reply.status(201).send()
