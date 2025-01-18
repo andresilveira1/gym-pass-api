2a135bad1324f3357e89a69e8a71fa09636e5fb6
André Silveira <andre.silveira1silva@gmail.com>
2025-01-17 22:44:40 -0300

    test: unitary, password with hash

---
+++ src/use-cases/create-account.spec.ts
@@ -0,0 +1,36 @@
+import { describe, expect, it } from 'vitest'
+import { CreateAccountUseCase } from './create-account'
+import { compare } from 'bcryptjs'
+
+describe('Register Use Case', () => {
+  it('should hash user password upon registration', async () => {
+    const createAccountUseCase = new CreateAccountUseCase({
+      async findByEmail(email) {
+        return null
+      },
+
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      },
+    })
+
+    const { user } = await createAccountUseCase.execute({
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password: '123456',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      user.password_hash
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+})
