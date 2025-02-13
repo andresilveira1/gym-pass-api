127ed0d8bfc86af38417be14933e7c3bca92dc77
André Silveira <andre.silveira1silva@gmail.com>
2025-02-13 20:00:52 -0300

    refactor: add Factory Pattern to use cases

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,8 +1,8 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
 import * as zod from 'zod'
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { AuthenticateUseCase } from '@/use-cases/authenticate'
+
 import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
 
 const authenticateBodySchema = zod.object({
   email: zod.string().email(),
@@ -13,8 +13,7 @@ export async function authenticate(req: FastifyRequest, reply: FastifyReply) {
   const { email, password } = authenticateBodySchema.parse(req.body)
 
   try {
-    const prismaUsersRepository = new PrismaUsersRepository()
-    const authenticateUseCase = new AuthenticateUseCase(prismaUsersRepository)
+    const authenticateUseCase = makeAuthenticateUseCase()
 
     await authenticateUseCase.execute({ email, password })
   } catch (err) {
--- src/http/controllers/create-account.ts
+++ src/http/controllers/create-account.ts
@@ -1,8 +1,8 @@
-import { CreateAccountUseCase } from '@/use-cases/create-account'
 import type { FastifyReply, FastifyRequest } from 'fastify'
 import * as zod from 'zod'
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+
 import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
+import { makeRegisterUseCase } from '@/use-cases/factories/make-create-account-use-case-'
 
 const registerBodySchema = zod.object({
   name: zod.string(),
@@ -14,8 +14,7 @@ export async function createAccount(req: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(req.body)
 
   try {
-    const prismaUsersRepository = new PrismaUsersRepository()
-    const createAccountUseCase = new CreateAccountUseCase(prismaUsersRepository)
+    const createAccountUseCase = makeRegisterUseCase()
 
     await createAccountUseCase.execute({ name, email, password })
   } catch (err) {
